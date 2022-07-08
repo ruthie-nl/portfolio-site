446e3dc59a1645f4b1b556c14ba49951164fe226
ruthie-nl <104361656+ruthie-nl@users.noreply.github.com>
2022-07-08 14:00:13 +0100

    Add files via upload

---
+++ time.py
@@ -0,0 +1,14 @@
+import time
+reminder = user.ask("what shall i remind you about?")
+print("what shall i remind you about?")
+text = str(input())
+
+
+print("in how many minutes")
+local_time = float(input())
+
+local_time = local_time * 60
+
+time.sleep(local_time)
+
+print(text)
